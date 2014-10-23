8fe3da2a9fbe735a774e9ce2c4b4d371ecdefd44
Fred Myers <fred@fred-myers.com>
2014-10-23 16:40:55 -0400

    Added the ability to buy lemons and ice cubes and have the UI update.

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -21,11 +21,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var iceCubesInBrewLabel: UILabel!
     @IBOutlet weak var todaysForecastImageView: UIImageView!
     
+    //MARK: - Variable
+    
+    var usersMoney = 10
+    var lemonsInInventory = 1
+    var iceCubesInInventory = 1
+    var lemonsToBuy = 0
+    var iceCubesToBuy = 0
+    
+    //MARK: - Constants
+    
+    let kCostPerLemon = 2
+    let kCostPerIceCube = 1
+    
+    
     //MARK: - Main Code
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.updateMainScree()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,15 +52,19 @@ class ViewController: UIViewController {
     //MARK: - IBActions
     
     @IBAction func addLemonsToInventoryButtonPressed(sender: UIButton) {
+        self.inventoryUpdate(sender)
     }
     
     @IBAction func removeLemonsFromInventoryButtonPressed(sender: UIButton) {
+        self.inventoryUpdate(sender)
     }
     
     @IBAction func addIceCubesToInventoryButtonPressed(sender: UIButton) {
+        self.inventoryUpdate(sender)
     }
 
     @IBAction func removeIceCubesFromInventoryButtonPressed(sender: UIButton) {
+        self.inventoryUpdate(sender)
     }
 
     @IBAction func addLemonsToBrewButtonPressed(sender: UIButton) {
@@ -60,6 +80,46 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startTheDayButtonPressed(sender: UIButton) {
+    }
+    
+    //MARK: - Helper Methods
+    
+    func inventoryUpdate (buttonPressed: UIButton) {
+        
+        switch buttonPressed.tag {
+        case 1:
+            //Add a lemon to inventory
+            self.usersMoney -= kCostPerLemon
+            self.lemonsInInventory += 1
+            self.lemonsToBuy += 1
+        case 2:
+            //Remove a lemon from inventory
+            self.usersMoney += kCostPerLemon
+            self.lemonsInInventory -= 1
+            self.lemonsToBuy -= 1
+        case 3:
+            //Add ice cube to inventory
+            self.usersMoney -= kCostPerIceCube
+            self.iceCubesInInventory += 1
+            self.iceCubesToBuy += 1
+        case 4:
+            self.usersMoney += kCostPerIceCube
+            self.iceCubesInInventory -= 1
+            self.iceCubesToBuy -= 1
+        default:
+            println("Error")
+        }
+        
+        self.updateMainScree()
+        
+    }
+    
+    func updateMainScree() {
+        self.moneyYouHaveLabel.text = "$\(self.usersMoney)"
+        self.lemonsInInventoryLabel.text = "\(self.lemonsInInventory)"
+        self.iceCubesInInventoryLabel.text = "\(self.iceCubesInInventory)"
+        self.lemonsToPurchaseLabel.text = "\(self.lemonsToBuy)"
+        self.iceCubesToPurchaseLabel.text = "\(self.iceCubesToBuy)"
     }
     
     
