6015d53f979a2368773d24376131753bbd7cee85
Fred Myers <fred@fred-myers.com>
2014-10-27 16:20:29 -0400

    Setup Customer class

---
+++ LemonadeStand/Customer.swift
@@ -0,0 +1,13 @@
+//
+//  Customer.swift
+//  LemonadeStand
+//
+//  Created by Fredrick Myers on 10/27/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+import Foundation
+
+class Customer {
+    var tastePreference = 1.0
+}
